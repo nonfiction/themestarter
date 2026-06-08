af0f57f1d2dbdb2dcc0da6e2bb6688878d8e0949
Jon Suderman <jon@suderman.net>
2026-06-08 13:15:34 -0600

    style(theme): reformat php-cs-fixer config to 2-space indent
    Indent the array entries in theme/.php-cs-fixer.dist.php with two
    spaces instead of four to match the rest of the project style. No
    rules, paths, or behavior change.

--- theme/.php-cs-fixer.dist.php
+++ theme/.php-cs-fixer.dist.php
@@ -6,29 +6,29 @@ use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
 $rootFiles = array_filter([
-    __DIR__ . '/functions.php',
-    __DIR__ . '/index.php',
-    __DIR__ . '/header.php',
-    __DIR__ . '/footer.php',
-    __DIR__ . '/sidebar.php',
+  __DIR__ . '/functions.php',
+  __DIR__ . '/index.php',
+  __DIR__ . '/header.php',
+  __DIR__ . '/footer.php',
+  __DIR__ . '/sidebar.php',
 ], 'file_exists');
 
 $finder = (new Finder())
     ->files()
     ->name('*.php')
     ->in([
-        __DIR__ . '/app',
-        __DIR__ . '/config',
-        __DIR__ . '/src',
+      __DIR__ . '/app',
+      __DIR__ . '/config',
+      __DIR__ . '/src',
     ])
     ->append($rootFiles)
     ->exclude([
-        'assets/dist',
-        'build',
-        'coverage',
-        'dist',
-        'node_modules',
-        'vendor',
+      'assets/dist',
+      'build',
+      'coverage',
+      'dist',
+      'node_modules',
+      'vendor',
     ]);
 
 return (new Config())
@@ -36,35 +36,35 @@ return (new Config())
     ->setRiskyAllowed(false)
     ->setUnsupportedPhpVersionAllowed(true)
     ->setRules([
-        '@PSR12' => true,
-        'array_indentation' => true,
-        'array_syntax' => ['syntax' => 'short'],
-        'binary_operator_spaces' => ['default' => 'single_space'],
-        'blank_line_before_statement' => [
-            'statements' => [
-                'break',
-                'continue',
-                'declare',
-                'return',
-                'throw',
-                'try',
-            ],
+      '@PSR12' => true,
+      'array_indentation' => true,
+      'array_syntax' => ['syntax' => 'short'],
+      'binary_operator_spaces' => ['default' => 'single_space'],
+      'blank_line_before_statement' => [
+        'statements' => [
+          'break',
+          'continue',
+          'declare',
+          'return',
+          'throw',
+          'try',
         ],
-        'concat_space' => ['spacing' => 'one'],
-        'no_extra_blank_lines' => true,
-        'no_unused_imports' => true,
-        'ordered_imports' => [
-            'imports_order' => ['class', 'function', 'const'],
-            'sort_algorithm' => 'alpha',
+      ],
+      'concat_space' => ['spacing' => 'one'],
+      'no_extra_blank_lines' => true,
+      'no_unused_imports' => true,
+      'ordered_imports' => [
+        'imports_order' => ['class', 'function', 'const'],
+        'sort_algorithm' => 'alpha',
+      ],
+      'single_quote' => true,
+      'trailing_comma_in_multiline' => [
+        'elements' => [
+          'arguments',
+          'arrays',
+          'parameters',
         ],
-        'single_quote' => true,
-        'trailing_comma_in_multiline' => [
-            'elements' => [
-                'arguments',
-                'arrays',
-                'parameters',
-            ],
-        ],
-        'yoda_style' => false,
+      ],
+      'yoda_style' => false,
     ])
     ->setFinder($finder);
