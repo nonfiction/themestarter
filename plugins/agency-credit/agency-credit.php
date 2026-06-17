54c19384b23deb34ff13de2fe143a21263c938a1
Jon Suderman <jon@suderman.net>
2026-06-17 15:56:16 -0600

    feat(plugins): add in-repo agency-credit footer plugin
    Add a new WordPress plugin under plugins/agency-credit/ that renders a
    small 'handcrafted by nonfiction studios' credit in the site footer,
    with inline styling scoped to the .agency-credit class.
    
    Register the plugin in nf.json as an in-repo source (slug 'agency-credit',
    source 'repo') so the deploy tooling can pick it up from this repository
    alongside the other WordPress plugins.

---
+++ plugins/agency-credit/agency-credit.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin Name: Agency Credit
+ * Description: Adds an agency credit to the site footer.
+ * Version: 0.1.0
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+add_action('wp_footer', function (): void {
+    ?>
+    <div class="agency-credit">
+        handcrafted by
+        <a href="https://www.nonfiction.ca/" target="_blank" rel="noopener">
+            nonfiction studios
+        </a>
+    </div>
+    <?php
+}, 100);
+
+add_action('wp_enqueue_scripts', function (): void {
+    wp_register_style('agency-credit', false, [], '0.1.0');
+    wp_enqueue_style('agency-credit');
+
+    wp_add_inline_style('agency-credit', '
+        .agency-credit {
+            padding: 1rem;
+            font-size: 0.875rem;
+            text-align: center;
+        }
+    ');
+});
